bdd015aab34f6d6c5e3a631d7549c355803a9d2e
update-image-hash <nobody@jimpick.com>
2024-02-27 22:24:57 -0800

    Add knative service using dnsfun image

--- hexcamp-dns/deploy.sh
+++ hexcamp-dns/deploy.sh
@@ -1,14 +1,14 @@
 #! /bin/bash
 
-. ../../../.env
+. ../.env
 
 echo GITHUB_PAT $GITHUB_PAT
 
-argocd repo add https://github.com/jimpick/localnet-farm.git --username jimpick --password $GITHUB_PAT --upsert
+argocd repo add https://github.com/hexcamp/hexcamp-argocd.git --username jimpick --password $GITHUB_PAT --upsert
 
-argocd app create localnet-farm-5-workloads \
+argocd app create hexcamp-dns \
   --upsert \
-  --repo https://github.com/jimpick/localnet-farm.git \
-  --path prototypes/localnet-farm-5/argocd-workloads \
+  --repo https://github.com/hexcamp/hexcamp-argocd.git \
+  --path hexcamp-dns \
   --dest-name localnet-farm-5 \
   --dest-namespace default
