34c9e08318bb60a5033f1bf459f73586f9c96c6a
Jim Pick <jim@jimpick.com>
2025-03-30 18:33:04 -0700

    Deploy ipfs to nuc

---
+++ ipfs/seahex/gkgv6/nuc/deploy.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+
+. ../../.env
+
+CLUSTER=nuc
+
+echo GITHUB_PAT $GITHUB_PAT
+
+NS=gkgv6
+
+kubectl --context $CLUSTER create ns $NS
+
+argocd repo add https://github.com/hexcamp/hexcamp-argocd.git --username jimpick --password $GITHUB_PAT --upsert
+
+argocd app create $CLUSTER-repair-eventing \
+  --upsert \
+  --repo https://github.com/hexcamp/hexcamp-argocd \
+  --path ipfs/seahex/gkgv6/nuc \
+  --dest-name $CLUSTER \
+  --dest-namespace $NS
