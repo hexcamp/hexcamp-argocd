b4ea5afffe524ec9f32acf756283a4affde7fa5f
Jim Pick <jim@jimpick.com>
2025-04-14 13:56:25 -0700

    No special symbols in password

--- ipfs/seahex/gkgv6/ipfs-cluster-secrets/gen-secrets.sh
+++ ipfs/seahex/gkgv6/ipfs-cluster-secrets/gen-secrets.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
 
 echo "CLUSTER_SECRET=$(openssl rand -hex 32)" > .env
-echo "CLUSTER_RESTAPI_BASICAUTHCREDENTIALS='{\"admin\": \"$(pwgen -y 16)\"}'" >> .env
+echo "CLUSTER_RESTAPI_BASICAUTHCREDENTIALS='{\"admin\": \"$(pwgen 16)\"}'" >> .env
