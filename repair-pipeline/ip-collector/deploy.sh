ba4bb00abe696849341f9c79a3df8202a1763588
Jim Pick <jim@jimpick.com>
2025-05-26 12:39:29 -0700

    Add service to collect ips from tofu

---
+++ repair-pipeline/ip-collector/deploy.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+. ../../.env
+
+CLUSTER=ryzen9
+
+echo GITHUB_PAT $GITHUB_PAT
+
+#kubectl --context $CLUSTER create ns repair
+
+argocd repo add https://github.com/hexcamp/hexcamp-argocd.git --username jimpick --password $GITHUB_PAT --upsert
+
+argocd app create $CLUSTER-repair-ip-collector \
+  --upsert \
+  --repo https://github.com/hexcamp/hexcamp-argocd \
+  --path repair-pipeline/ip-collector \
+  --dest-name $CLUSTER \
+  --dest-namespace repair
