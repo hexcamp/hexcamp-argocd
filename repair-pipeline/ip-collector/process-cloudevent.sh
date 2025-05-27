44e288e422cb278b4e727312c96389c08e6e4f28
Jim Pick <jim@jimpick.com>
2025-05-27 10:02:14 -0700

    Update script to make ips.json

--- repair-pipeline/ip-collector/process-cloudevent.sh
+++ repair-pipeline/ip-collector/process-cloudevent.sh
@@ -1,4 +1,15 @@
 #! /bin/sh
 
+set -euxo pipefail
+
 echo "Mock process-cloudevent.sh"
+touch /ips/ips.json
+OLD=$(md5sum /ips/ips.json)
+NEW=$(md5sum /tmp/cloudevent.json)
+
+echo "Old: $OLD New: $OLD"
+if [ "$OLD" != "$NEW" ]; then
+  cp /tmp/cloudevent.json /ips/ips.json
+  echo FIXME: Call Sink
+fi
 echo "Done."
