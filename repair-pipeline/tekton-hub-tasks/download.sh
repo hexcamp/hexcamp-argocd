7278a264af100e84b50195e17c79c1914857e19b
Jim Pick <jim@jimpick.com>
2025-05-28 12:42:00 -0700

    Add generate-build-id task

--- repair-pipeline/tekton-hub-tasks/download.sh
+++ repair-pipeline/tekton-hub-tasks/download.sh
@@ -3,3 +3,5 @@
 wget -r https://api.hub.tekton.dev/v1/resource/tekton/task/git-clone/0.9/raw
 
 wget -r https://api.hub.tekton.dev/v1/resource/tekton/task/cloudevent/0.1/raw
+
+wget -r https://api.hub.tekton.dev/v1/resource/tekton/task/generate-build-id/0.1/raw
