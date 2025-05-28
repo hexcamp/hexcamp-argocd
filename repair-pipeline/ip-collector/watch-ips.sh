7012640c05a807783859845966c35a9ccf164f07
Jim Pick <jim@jimpick.com>
2025-05-28 11:44:31 -0700

    Remove old tutorial

--- repair-pipeline/ip-collector/watch-ips.sh
+++ repair-pipeline/ip-collector/watch-ips.sh
@@ -3,6 +3,7 @@
 CONTEXT=ryzen9
 
 while true; do
-  kubectl --context $CONTEXT -n repair logs -f -l serving.knative.dev/service=ip-collector
-  sleep 1
+  kubectl --context $CONTEXT -n repair logs -f --tail 100 -l serving.knative.dev/service=ip-collector 2> /dev/null
+  sleep 3
+  echo -n .
 done
