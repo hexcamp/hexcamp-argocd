ad558b8db7c46c37b663d65e4e7be64069ff9122
Jim Pick <jim@jimpick.com>
2025-05-31 13:35:47 -0700

    Fix dir name

--- repair-pipeline/hackmd-updater/deploy.sh
+++ repair-pipeline/hackmd-updater/deploy.sh
@@ -13,6 +13,6 @@ argocd repo add https://github.com/hexcamp/hexcamp-argocd.git --username jimpick
 argocd app create $CLUSTER-hackmd-updater \
   --upsert \
   --repo https://github.com/hexcamp/hexcamp-argocd \
-  --path repair-pipeline/hackmd-updater \
+  --path repair-pipeline/hackmd-updater-secrets \
   --dest-name $CLUSTER \
   --dest-namespace repair
