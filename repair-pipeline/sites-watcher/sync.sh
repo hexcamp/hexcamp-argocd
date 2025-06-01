fc3de076fa228911c0d139739baf52d8fc9e1663
Jim Pick <jim@jimpick.com>
2025-06-01 12:38:56 -0700

    Clone 2 commits

--- repair-pipeline/sites-watcher/sync.sh
+++ repair-pipeline/sites-watcher/sync.sh
@@ -4,6 +4,8 @@
 
 CLUSTER=ryzen9
 
+tkn -n repair pipelinerun delete --all -f
+
 argocd app sync $CLUSTER-repair-sites-watcher
 
 kubectl --context ryzen9 -n repair delete pods --wait=false -l eventlistener=sites-github-clone-listener
