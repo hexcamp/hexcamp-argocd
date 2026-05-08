7805125f9dff3e2f70205e62192ff2e1c9b4cefe
Jim Pick <jim@jimpick.com>
2026-05-08 16:59:09 -0700

    Add project that uses Tekton for the VanHex Newshub

---
+++ newshub/vanhex/sync.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+. ../../.env
+
+CLUSTER=ryzen9
+
+tkn -n newshub-vanhex pipelinerun delete --all -f
+
+argocd app sync $CLUSTER-newshub-vanhex
+
+#kubectl --context ryzen9 -n newshub-vanhex delete pods --wait=false -l eventlistener=sites-github-clone-listener
+
